f3654a5d8aae944a6a842b00ffbe75c983b0a1d9
phoenixveracity <phoenix_veracity@yahoo.com>
2018-10-16 17:38:04 +1100

    Updates

---
+++ precache-manifest.35fd434ac205ef85551be835cc1882a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "d9137f7cf46a8867b8ca",
+    "url": "/website/static/js/main.d9137f7c.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/website/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "d9137f7cf46a8867b8ca",
+    "url": "/website/static/css/main.9e2d02f3.chunk.css"
+  },
+  {
+    "revision": "a4342d208e67f45c5cca4dc1986f1c29",
+    "url": "/website/index.html"
+  }
+];
